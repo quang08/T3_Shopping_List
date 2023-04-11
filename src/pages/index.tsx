657d3d49a5a91a1c6a6646825239dfa6bba4362f
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-04-11 07:09:32 +0700

    fixed root.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
-import { api } from "~/utils/api";
+// import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
